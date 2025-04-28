c80822ddc8dbfe59fb485c5fb2d5a1ac3cb11afa
JoseVitor0 <144168385+JoseVitor0@users.noreply.github.com>
2025-04-28 20:08:10 -0300

    algoritmos e imagens

---
+++ deteccao-pontos3.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+# Carregar a imagem em escala de cinza
+imagem = cv2.imread('campnou.jpg', cv2.IMREAD_GRAYSCALE)
+
+harris = cv2.cornerHarris(imagem, blockSize=6, ksize=25, k=0.04)
+
+harris = cv2.dilate(harris, None)
+
+imagem[harris > 0.01 * harris.max()] = [255]
+
+cv2.imshow("Harris Corner", imagem)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
